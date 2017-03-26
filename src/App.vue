a656eb4acbbf308f60ab99556f0b12c1136f8e4b
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-03-26 20:42:11 +0200

    Imports the bootsrap and the jQuery

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
   </div>
 </template>
 
+<!-- declare these external scripts first before exporting the app component -->
+<!-- JQuery required by bootstrap and admin lte -->
+<script src="./assets/vendor/jQuery/jquery-2.2.3.min.js" charset="utf-8"></script>
+
 <script>
   import Sidebar from './components/Sidebar/Sidebar.vue'
 
@@ -17,6 +21,4 @@
   }
 </script>
 
-<style>
-
-</style>
+<style src="./assets/vendor/bootstrap/css/bootstrap.min.css"></style>
