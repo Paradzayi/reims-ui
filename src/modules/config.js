e1dfc55fb99c09f204361dfcc98b27035f9693fe
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-03-27 15:45:42 +0200

    initialise mock sidebar menus

---
+++ src/modules/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  sidebar: {
+    menus: [
+      {
+        title: 'Home',
+        url: '/home'
+      },
+      {
+        title: 'Settings',
+        url: '/settings'
+      },
+      {
+        title: 'Students',
+        url: '/students'
+      },
+      {
+        title: 'Accounts',
+        url: '/accounts'
+      },
+      {
+        title: 'Danger zone',
+        url: '/danger zone'
+      },
+      {
+        title: 'Sorroo',
+        url: '/soro'
+      }
+    ]
+  }
+}
