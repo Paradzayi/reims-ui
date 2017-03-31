9d0dddb3bc0462ae8050d5e5f92cec233cdda705
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-03-31 16:38:49 +0200

    serves the semantic-ui.min.js to any clients

--- src/modules/assets.js
+++ src/modules/assets.js
@@ -14,4 +14,10 @@ res.sendFile(semantic_ui_css_dir);
 
 })
 
+router.get('/js/semantic-ui.min.js', (req, res, next) => {
+  // define the path to get the semantic ui css
+  let semantic_ui_js_dir = path.join(assetsFolder, 'vendor/semantic-ui/js/semantic-ui.min.js');
+  res.sendFile(semantic_ui_js_dir);
+
+})
 module.exports = router;
