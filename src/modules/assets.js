6dc6b721cea18c054c6bcc3755e0f94d8b370086
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-03-31 16:31:31 +0200

    gets the semantic ui css and returns it via HTTP

---
+++ src/modules/assets.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var assetsFolder = getAssetsFolder();
+
+function getAssetsFolder() {
+  return  __dirname.substring(0, __dirname.lastIndexOf('/')) + '/assets';
+}
+router.get('/css/semantic-ui.min.css', (req, res, next) => {
+
+// define the path to get the semantic ui css
+let semantic_ui_css_dir = path.join(assetsFolder, 'vendor/semantic-ui/css/semantic-ui.min.css');
+res.sendFile(semantic_ui_css_dir);
+
+})
+
+module.exports = router;
