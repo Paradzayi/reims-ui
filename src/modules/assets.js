1ddf1be110fb87e231c058bc80907bca26ff7981
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-04-02 09:39:42 +0200

    Serves the assets to clients

--- src/modules/assets.js
+++ src/modules/assets.js
@@ -9,15 +9,37 @@ function getAssetsFolder() {
 router.get('/css/semantic-ui.min.css', (req, res, next) => {
 
 // define the path to get the semantic ui css
-let semantic_ui_css_dir = path.join(assetsFolder, 'vendor/semantic-ui/css/semantic-ui.min.css');
+let semantic_ui_css_dir = path.join(assetsFolder, 'vendor/semantic-ui/semantic-ui.min.css');
 res.sendFile(semantic_ui_css_dir);
 
 })
 
 router.get('/js/semantic-ui.min.js', (req, res, next) => {
   // define the path to get the semantic ui css
-  let semantic_ui_js_dir = path.join(assetsFolder, 'vendor/semantic-ui/js/semantic-ui.min.js');
+  let semantic_ui_js_dir = path.join(assetsFolder, 'vendor/semantic-ui/semantic-ui.min.js');
   res.sendFile(semantic_ui_js_dir);
 
 })
+
+router.get('/js/jquery-2.2.3.min.js', (req, res, next) => {
+  // define the path to get the semantic ui css
+  let jquery_dir = path.join(assetsFolder, 'vendor/jQuery/jquery-2.2.3.min.js');
+  res.sendFile(jquery_dir);
+
+})
+
+router.get('/css/themes/default/assets/fonts/icons.ttf', (req, res, next) => {
+  // define the path to get the semantic ui css
+  let icons_ttf_dir = path.join(assetsFolder, 'vendor/semantic-ui/themes/default/assets/fonts/icons.ttf');
+  res.sendFile(icons_ttf_dir);
+
+})
+
+router.get('/css/themes/default/assets/fonts/icons.woff', (req, res, next) => {
+  // define the path to get the semantic ui css
+  let icons_woff_dir = path.join(assetsFolder, 'vendor/semantic-ui/themes/default/assets/fonts/icons.woff');
+  res.sendFile(icons_woff_dir);
+
+})
+
 module.exports = router;
