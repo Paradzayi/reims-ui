8222bd48c5efc3a9a4468078c29e1131c428962c
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-04-02 10:39:36 +0200

    Initialise the basic views

---
+++ src/components/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ui secondary orange pointing menu">
+    <router-link to="/" class="active item">Home</router-link>
+    <router-link to="/stands" class="item">Stands</router-link>
+    <router-link to="/clients" class="item">Clients</router-link>
+    <router-link to="/finances" class="item">Finances</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/views/Finances.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    <p>finances</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
