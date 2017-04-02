bfdc73e299f3f8fbf10901923faf2ea7fa56e556
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-04-02 10:40:44 +0200

    Import the views and assign them to respective routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Hello from '@/views/Hello'
+
+// import the vue views to render as views
+import Login from '@/views/Login'
+import Stands from '@/views/Stands'
+import Clients from '@/views/Clients'
+import Finances from '@/views/Finances'
 
 Vue.use(Router)
 
@@ -10,6 +16,25 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/Clients',
+      component: Clients,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/stands',
+      component: Stands
+    },
+    {
+      path: '/finances',
+      component: Finances
     }
   ]
 })
