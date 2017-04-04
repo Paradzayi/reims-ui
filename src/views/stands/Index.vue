bc4945d8137d66ccbd95116bac66165825db62c8
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-04-04 13:42:13 +0200

    Make stands menu higlight when selected

--- src/views/stands/Index.vue
+++ src/views/stands/Index.vue
@@ -4,10 +4,10 @@
 
         <!--the menu that poins to the sub routes of the stands view -->
         <div class="ui purple secondary pointing container menu">
-          <router-link to="/stands" class="active item">All</router-link>
-          <router-link to="/stands/available" class="item">Available</router-link>
-          <router-link to="/stands/reserved" class="item">Reserved</router-link>
-          <router-link to="/stands/sold" class="item">Sold</router-link>
+          <a href="#/stands" id="all" @click="selectMenu('all')" class="active item">All</a>
+          <a href="#/stands/available" id="available" @click="selectMenu('available')" class="item">Available</a>
+          <a href="#/stands/reserved" id="reserved" @click="selectMenu('reserved')" class="item">Reserved</a>
+          <a href="#/stands/sold" id="sold" @click="selectMenu('sold')" class="item">Sold</a>
         </div>
 
         <!-- the subroutes to the stands view will be rendered here -->
@@ -18,11 +18,34 @@
 </template>
 
 <script>
+import menu from '@/modules/menu'
+
 export default {
   data () {
     return {
-      number: 0
+      menus: [
+        'all', 'available', 'reserved', 'sold'
+      ],
+      defaultMenu: 'all',
+      parentView: 'stands'
     }
+  },
+  methods: {
+    selectMenu (value) {
+      menu.changeMenuClases(value, this.menus, this.$el)
+    }
+  },
+  mounted () {
+    let path = this.$route.path
+    let selectedMenu = path.substring(path.lastIndexOf('/') + 1, path.length)
+
+    // check if the selected menu points to the /. if it does manually set it to the default menu
+    selectedMenu = selectedMenu === this.parentView
+      ? this.defaultMenu
+      : selectedMenu
+
+    // then higlight the selected menu
+    menu.changeMenuClases(selectedMenu, this.menus, this.$el)
   }
 }
 </script>
