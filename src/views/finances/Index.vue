36076ddf7ae35ac9d0554ff7b742c7b566bed267
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-04-04 13:42:44 +0200

    make finances menu highlight when selected

--- src/views/finances/Index.vue
+++ src/views/finances/Index.vue
@@ -4,8 +4,22 @@
 
         <!--the menu that poins to the sub routes of the stands view -->
         <div class="ui purple secondary pointing container menu">
-          <router-link to="/finances" class="active item">History</router-link>
-          <router-link to="/finances/summary" class="item">Summary</router-link>
+          <a
+            id="history"
+            href="#/finances"
+            @click="selectMenu('history')"
+            class="active item">
+            History
+          </a>
+
+          <a
+            id="summary"
+            href="#/finances/summary"
+            @click="selectMenu('summary')"
+            class="item">
+            Summary
+          </a>
+
         </div>
 
         <!-- the subroutes to the stands view will be rendered here -->
@@ -16,7 +30,35 @@
 </template>
 
 <script>
+import menu from '@/modules/menu'
+
 export default {
+  data () {
+    return {
+      menus: [
+        'history', 'summary'
+      ],
+      defaultMenu: 'history',
+      parentView: 'finances'
+    }
+  },
+  methods: {
+    selectMenu (value) {
+      menu.changeMenuClases(value, this.menus, this.$el)
+    }
+  },
+  mounted () {
+    let path = this.$route.path
+    let selectedMenu = path.substring(path.lastIndexOf('/') + 1, path.length)
+
+    // check if the selected menu points to the /. if it does manually set it to the default menu
+    selectedMenu = selectedMenu === this.parentView
+      ? this.defaultMenu
+      : selectedMenu
+
+    // then higlight the selected menu
+    menu.changeMenuClases(selectedMenu, this.menus, this.$el)
+  }
 }
 </script>
 
