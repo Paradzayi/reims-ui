483793d16958f50d2b8ab1a88cbe04a02fe568b5
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-04-20 12:18:10 +0200

    Add the map functionality (Not working)
    There is an error in the code in this commit.
    I hope connecting to the internet will help
    solve the error. Mapbox can be a real b**ch sometimes

---
+++ src/views/Map.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class='ui stackable container grid'>
+    <div class='two wide column'>
+      <div class='ui vertical  red  pointing fluid  menu'>
+        <div class='active item'>
+          <a href='#'>Cities</a>
+        </div>
+        <div class='item'>
+          <a href='#'>Cadastre</a>
+        </div>
+      </div>
+    </div>
+
+    <div class='twelve wide column'>
+      <div id='mapview' class="ui fluid orange inverted segment">
+        <div class="ui center aligned purple segment">
+          This is where the map is to be if it works out.
+          <br>
+          <div class="ui orange message"><i class="info icon"></i>Try connecting to the internet to see if the problem is not related to the lack of internet</div>
+          <br>
+          <i class="massive orange map icon"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+import axios from 'axios'
+import config from '@/modules/config'
+let ApiConfig = config.api
+
+export default {
+  data () {
+    return {
+      geojson: {}
+    }
+  },
+  created () {
+    axios.get(ApiConfig.baseUrl + '/api/cities')
+      .then((response) => {
+        console.log(response.data.data[0])
+        this.geojson = response.data.data[0]
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+  mounted () {
+    this.fetchMapData()
+  },
+
+  methods: {
+
+    initMap: function () {
+      var _this = this
+      // initialisation options for mapbox
+      let mapOptions = {
+        container: 'mapview', // container id
+      //  style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+        center: [30.15, -18.13], // starting position
+        zoom: 9, // starting zoom
+        minZoom: 8,
+        maxBounds: [[30.08, -18.18], [30.18, -18.10]]
+      }
+
+      // apply the accessToken and start the map
+      mapboxgl.accessToken = 'pk.eyJ1IjoicmFpa28iLCJhIjoiSy1KVE82byJ9.dcia_8lqXX8DsL8czQNB9A'
+      var map = new mapboxgl.Map(mapOptions)
+      console.log('loading the map.............')
+
+      map.on('load', addSources())
+
+      function addSources () {
+        console.log('inside the load event...')
+        // when the map loads add a new layer
+        // map.on('load', () => {
+        // add the city stands
+        console.log(map)
+        map.addSource('cities', {
+          type: 'geojson',
+          'data': _this.geojson
+        })
+
+        map.addLayer({
+          'id': 'stands',
+          'type': 'fill',
+          'source': 'stands',
+          'paint': {
+            'fill-color': 'white',
+            'fill-opacity': 0.7,
+            'fill-outline-color': 'sienna'
+          }
+        })
+
+        console.log('Done loading the map...')
+      }
+    //  })
+    },
+
+    fetchMapData () {
+      console.log('fetching......')
+      // fetch the geojson from server
+      axios.get(ApiConfig.baseUrl + '/api/stands?map=true')
+      .then(response => {
+        this.geojson = response.data.data[0]
+
+        // initialise the map.
+        this.initMap()
+      })
+      .catch(err => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang='css' scoped>
+  #mapview {
+    height: 500px;
+    width: 100%;
+  }
+</style>
