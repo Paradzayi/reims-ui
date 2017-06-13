076e848fbd3a47a0ad3ee7ddeb2be33f1f97021e
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-13 08:41:28 +0200

    Message required by BuyReserve Component

---
+++ src/components/Message.vue
@@ -0,0 +1,34 @@
+<template >
+  <div :class="messageCss" v-if="message">
+    <i :class="iconCss"></i>{{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message', 'type'],
+
+  computed: {
+    messageCss () {
+      return {
+        ui: true,
+        positive: this.type === 'info' || this.type === 'success',
+        negative: this.type === 'error',
+        message: true
+      }
+    },
+
+    iconCss () {
+      return {
+        check: this.type === 'success',
+        warning: this.type === 'error',
+        icon: true
+      }
+    }
+  }
+}
+
+</script>
+
+<style lang="css">
+</style>
