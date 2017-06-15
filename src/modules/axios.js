83343a90af2f264f61251c35abbdbbd08b1123e2
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-15 17:48:52 +0200

    Use correct url for api

--- src/modules/axios.js
+++ src/modules/axios.js
@@ -1,9 +1,9 @@
 const axios = require('axios')
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production'
-    ? 'https://reims-app.herokuapp.com/api'
-    : 'http://localhost:3000/api'
+  baseURL: process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/api'
+    : 'https://reims-app.herokuapp.com/api'
 })
 
 module.exports = instance
