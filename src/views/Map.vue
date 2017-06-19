c0a569e7dfc3e7287181af75af4f98b08e4f6426
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-19 16:48:25 +0200

    Add the MapSideMenu Component. Remove the old one

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -13,181 +13,19 @@
         @close="openBuySummary = false">
       </buy-summary>
       <!-- The menu-->
-      <div class='ui vertical  orange fluid mini menu'>
-        <div class="ui top attached orange segment">
-          <button class="ui right floated mini red icon button" @click="clearAllFeatures">
-            <i class="remove icon"></i>
-            Clear All
-          </button>
-          <div class="header">
-            Select to show in map
-          </div>
-        </div>
+      <MapSideMenu
+        :allStands="allStands"
+        :availableStands="availableStands"
+        :reservedStands="reservedStands"
+        :soldStands="soldStands"
 
-        <!-- The reservedStads menu item-->
-        <div
-          :class="{active: allStands.features, item: true, link: true}">
-
-          <!-- Fetch reserved items when you click here -->
-          <a
-            @click="fetchAllStands">
-            All Stands
-          </a>
-
-          <!-- show the user a red zero if there are no reserved items -->
-          <div
-            class="ui red basic label"
-            v-if = "allStands.type && !!allStands.features === false">
-            0
-          </div>
-
-          <!-- show the user the number of all stands -->
-          <div
-            class="ui brown basic label"
-            v-if = "allStands.features">
-            {{allStands.features.length }}
-          </div>
-
-          <!-- Allow the user to toggle the layers if they are present -->
-          <div
-            v-if = "allStands.features">
-            <p>
-              <div class="ui slider checkbox">
-                <input
-                  type="checkbox"
-                  :checked="isLayerInMap('allStands')"
-                  @click="toggleLayer('allStands')">
-                <label>Show in map</label>
-              </div>
-            </p>
-          </div>
-
-        </div><!--/ The reservedStands menu item -->
-
-        <!-- The availableStads menu item-->
-        <div
-          :class="{active: availableStands.features, item: true, link: true}">
-
-          <!-- Fetch reserved items when you click here -->
-          <a
-            @click="fetchavailableStands">
-            Available Stands
-          </a>
-
-          <!-- show the user a red zero if there are no available items -->
-          <div
-            class="ui red basic label"
-            v-if = "availableStands.type && !!availableStands.features === false">
-            0
-          </div>
-
-          <!-- show the user the number of available items -->
-          <div
-            class="ui purple basic label"
-            v-if = "availableStands.features">
-            {{ availableStands.features.length }}
-          </div>
-
-          <!-- Allow the user to toggle the layers if they are present -->
-          <div
-            v-if = "availableStands.features">
-            <p>
-              <div class="ui slider checkbox">
-                <input
-                  type="checkbox"
-                  :checked="isLayerInMap('availableStands')"
-                  @click="toggleLayer('availableStands')">
-                <label>Show in map</label>
-              </div>
-            </p>
-          </div>
-
-        </div><!--/ The reservedStands menu item -->
-
-        <!-- The reservedStads menu item-->
-        <div
-          :class="{active: reservedStands.features, item: true, link: true}">
-
-          <!-- Fetch reserved items when you click here -->
-          <a
-            @click="fetchReservedStands">
-            Reserved Stands
-          </a>
-
-          <!-- show the user a red zero if there are no reserved items -->
-          <div
-            class="ui red basic label"
-            v-if = "reservedStands.type && !!reservedStands.features === false">
-            0
-          </div>
-
-          <!-- show the user the number of reserved items -->
-          <div
-            class="ui orange basic label"
-            v-if = "reservedStands.features">
-            {{ reservedStands.features.length }}
-          </div>
-
-          <!-- Allow the user to toggle the layers if they are present -->
-          <div
-            v-if = "reservedStands.features">
-            <p>
-              <div class="ui slider checkbox">
-                <input
-                  type="checkbox"
-                  :checked="isLayerInMap('reservedStands')"
-                  @click="toggleLayer('reservedStands')">
-                <label>Show in map</label>
-              </div>
-            </p>
-          </div>
-
-        </div><!--/ The reservedStands menu item -->
-
-        <!-- The reservedStads menu item-->
-        <div
-          :class="{active: soldStands.features, item: true, link: true}">
-
-          <!-- Fetch reserved items when you click here -->
-          <a
-            @click="fetchSoldStands">
-            Sold Stands
-          </a>
-
-
-          <!-- show the user a red zero if there are no sold items -->
-          <div
-            class="ui red basic label"
-            v-if = "soldStands.type && !!soldStands.features === false">
-            0
-          </div>
-
-          <!-- show the user the number of reserved items -->
-          <div
-            class="ui green basic label"
-            v-if = "soldStands.features">
-            {{ soldStands.features.length }}
-          </div>
-
-          <!-- Allow the user to toggle the layers if they are present -->
-          <div
-            v-if = "soldStands.features">
-            <p>
-              <div class="ui slider checkbox">
-                <input
-                  type="checkbox"
-                  :checked="isLayerInMap('soldStands')"
-                  @click="toggleLayer('soldStands')">
-                <label>Show in map</label>
-              </div>
-            </p>
-          </div>
-
-        </div><!--/ The soldStands menu item -->
-
-        <!-- Other menu items will come down here -->
-
-      </div>
+        @clearAllFeatures="clearAllFeatures"
+        @fetchAllStands="fetchAllStands"
+        @fetchAvailableStands="fetchAvailableStands"
+        @fetchReservedStands="fetchReservedStands"
+        @fetchSoldStands="fetchSoldStands"
+        @toggleLayer="toggleLayer">
+      </MapSideMenu>
 
       <!--Show categorised stands down here (only if there a stands to be zoomed to)
           so that user can zoom to them in the map -->
@@ -247,6 +85,7 @@ import mapboxgl from 'mapbox-gl'
 import polylabel from 'polylabel'
 import moment from 'moment'
 import BuySummary from '@/components/BuySummary'
+import MapSideMenu from '@/components/MapSideMenu'
 
 export default {
   /*
@@ -340,7 +179,10 @@ export default {
     }
   },
 
-  components: {BuySummary},
+  components: {
+    BuySummary,
+    MapSideMenu
+  },
   /*
     Code that is executed when the component has been compiled
     and ready to be used.
